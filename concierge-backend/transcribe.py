5472a11df828e27d86ea33845841c25f75904880
rorosaga <rodrigo.sagastegui9@gmail.com>
2024-07-17 16:12:15 -0400

    updated transcribe.py

--- concierge-backend/transcribe.py
+++ concierge-backend/transcribe.py
@@ -1,3 +1,12 @@
 import sys
 import os
 import whisper
+import time
+
+model = whisper.load_model("tiny")
+
+def transcribe(audio_path):
+    start_time = time.time()
+    result = model.transcribe(audio_path, language="es", fp16=False)
+    end_time = time.time()
+    print(f"Transcription took {end_time - start_time} seconds")
